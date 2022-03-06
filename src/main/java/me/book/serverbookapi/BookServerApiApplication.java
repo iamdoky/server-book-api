1799b4edd89a2af0ee05b2a1d54b4ee882a80529
doky <48897815+iamdoky@users.noreply.github.com>
2022-03-06 23:02:47 +0900

    스프링 API 서버 연습 시작

---
+++ src/main/java/me/book/serverbookapi/BookServerApiApplication.java
@@ -0,0 +1,13 @@
+package me.book.serverbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/book/serverbookapi/BookServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.book.serverbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
